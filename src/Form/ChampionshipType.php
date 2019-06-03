61aac6d221eb83f31f4d2d679107dcf0f9f335b3
janssens <janssensgaetan@gmail.com>
2019-06-03 12:39:51 +0200

    disabled date for pre-registrations at championship final

--- src/Form/ChampionshipType.php
+++ src/Form/ChampionshipType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Championship;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
